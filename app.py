e7b0531ae4f7b437e117247e3ba9306c50366760
Harang Ju <harang@mit.edu>
2024-11-06 11:36:11 -0500

    redirect if logged in

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
+if "prolific_id" in st.session_state and st.session_state["prolific_id"]:
+  st.switch_page("pages/instructions.py")
+  st.stop()
+
 _, col, _ = st.columns([1, 3, 1])
 
 with col:
@@ -8,6 +12,8 @@ with col:
   prolific_id = st.text_input("Enter your Prolific ID: ")
   start_clicked = st.button("Start Survey", disabled=not prolific_id)
 
-  if prolific_id and start_clicked:
+  if prolific_id:
     st.session_state["prolific_id"] = prolific_id
+
+  if prolific_id and start_clicked:
     st.switch_page("pages/instructions.py")
